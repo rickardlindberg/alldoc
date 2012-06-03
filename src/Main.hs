9b67735e620c19ac5bab778ef2e363afe4fd9bd6
Rickard Lindberg <ricli85@gmail.com>
2012-06-03 20:11:06 +0200

    sketch data structure for parsed docs

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+data DocPiece =
+      Namespace
+        { name      :: String
+        , url       :: String
+        , subPieces :: [DocPiece]
+        }
+    | DocPiece
+        { name      :: String
+        , url       :: String
+        }
+    deriving (Show)
+
 main :: IO ()
 main = putStrLn "not implemented yet"
