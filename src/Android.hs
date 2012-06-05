6d52fab9e11c7f422a086e1a49fd3d9b0c78db3a
Rickard Lindberg <ricli85@gmail.com>
2012-06-05 12:43:40 +0200

    refactor using Maybe monad

--- src/Android.hs
+++ src/Android.hs
@@ -1,25 +1,39 @@
 module Android where
 
+import Data.Maybe
 import Definitions
 import Text.HTML.TagSoup
 import Text.StringLike
 
 parseClass :: StringLike str => [Tag str] -> [DefTree]
-parseClass doc =
-       (prefixWithNamespace (parseClassNamespaceName doc)
-     . map ((`Definition` "") . toString . ((\(TagText x) -> x) . head) . head . sections isTagText)
-     . sections (~== "<span class=sympad>")
-     . head
-     . sections (~== "<div id=doc-content>"))
-       doc
+parseClass = fromJust . parseClassInner
 
-parseClassNamespaceName :: StringLike str => [Tag str] -> String
-parseClassNamespaceName =
-       firstText
-     . last
-     . sections (~== "<td class=jd-inheritance-class-cell")
-     . head
-     . sections (~== "<table class=jd-inheritance-table>")
+parseClassInner :: StringLike str => [Tag str] -> Maybe [DefTree]
+parseClassInner doc = do
+    ns <- maybeClassNamespace doc
+    rest <- maybeClass doc
+    return $ prefixWithNamespace ns rest
+
+maybeClass :: StringLike str => [Tag str] -> Maybe [DefTree]
+maybeClass doc = do
+     let a1 = sections (~== "<div id=doc-content>") doc
+     a2 <- maybeHead a1
+     let a3 = sections (~== "<span class=sympad>") a2
+     let a4 = map ((`Definition` "") . toString . ((\(TagText x) -> x) . head) . head . sections isTagText) a3
+     return a4
+
+maybeClassNamespace :: StringLike str => [Tag str] -> Maybe String
+maybeClassNamespace tags = do
+    let a1 = sections (~== "<table class=jd-inheritance-table>") tags
+    a2 <- maybeHead a1
+    let a3 = sections (~== "<td class=jd-inheritance-class-cell") a2
+    a4 <- maybeLast a3
+    let a5 = firstText a4
+    return a5
+
+maybeHead = listToMaybe
+
+maybeLast = listToMaybe . reverse
 
 firstText :: StringLike str => [Tag str] -> String
 firstText = toString . ((\(TagText x) -> x) . head) . head . sections isTagText
