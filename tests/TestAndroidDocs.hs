64a15aa3d359760b927673646f8dc03a37310b63
Rickard Lindberg <ricli85@gmail.com>
2012-06-05 11:18:48 +0200

    move Matrix.html

--- tests/TestAndroidDocs.hs
+++ tests/TestAndroidDocs.hs
@@ -10,5 +10,5 @@ import Test.Hspec.Monadic
 tests = describe "Android SDK extractor:" $
 
     it "finds method names" $ do
-        soup <- soupFromFile "sample-docs/android/android-Matrix.hml"
+        soup <- soupFromFile "sample-docs/android/Matrix.hml"
         parseClass soup `assertContains` "android.graphics.Matrix.setRotate"
