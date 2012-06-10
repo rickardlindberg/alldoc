22607f07021563b07f493956e9106819cedc6182
Rickard Lindberg <ricli85@gmail.com>
2012-06-10 15:42:38 +0200

    scan smaller set of files

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,6 @@ import qualified Scanner.Android as Android
 
 main :: IO ()
 main = do
-    let androidApiDocDir = "/home/rick/downloads/android-sdk-linux/docs/reference/"
+    let androidApiDocDir = "/home/rick/downloads/android-sdk-linux/docs/reference/android/graphics/drawable/"
     defs <- scanHtmlDocs androidApiDocDir Android.scanFile
     mapM_ putStrLn (namespaces defs)
